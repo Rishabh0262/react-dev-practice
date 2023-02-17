277e8e2755cffb67abab9235cd14fe71519a8fa0
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-02-18 01:54:26 +0530

    Memo is for Functional Component. (as same as PureComp for Class Component). we export React.memo(functional-ComponentName).

--- src/Components/ParentComp.js
+++ src/Components/ParentComp.js
@@ -1,9 +1,10 @@
-import React, { Component, PureComponent } from 'react'
+import React, { Component } from 'react'
 import RegularComp from './RegularComp'
 import PureComp from './PureComp'
+import MemoComp from './MemoComp'
 
 
-export class ParentComp extends PureComponent {
+export class ParentComp extends Component {
     constructor(props) {
       super(props)
     
@@ -24,8 +25,10 @@ export class ParentComp extends PureComponent {
     console.log("********** Parent Component Render *********")
     return (
       <div>ParentComp
-        <RegularComp name={this.state.name} />
-        <PureComp name={this.state.name} />
+        <MemoComp name={this.state.name} />
+
+        {/* <RegularComp name={this.state.name} />
+        <PureComp name={this.state.name} /> */}
       </div>
     )
   }
