8095ca54034e6ef91b8ac35c672edd0751051b49
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-02-17 10:13:29 +0530

    Example of using 'key' attribute in <React.Fragment>...</React.Fragment>

--- src/Components/Columns.js
+++ src/Components/Columns.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
 function Columns() {
-  return (
-        // <React.Fragment>
-        //     <td>Name</td>
-        //     <td>Rishabh</td>
-        // </React.Fragment>
-        
+    const items = []
 
-        <>
+    return (
+
+        <React.Fragment>
+            {
+                items.map(item => (
+                    <React.Fragment key={item.id}>
+                        <h1>Title</h1>
+                        <p>item.title</p>
+                    </React.Fragment>
+                ))
+            }
             <td>Name</td>
             <td>Rishabh</td>
-        </>
+        </React.Fragment>
+        
+
+        // <>
+        //     <td>Name</td>
+        //     <td>Rishabh</td>
+        // </>
     )
 }
 
