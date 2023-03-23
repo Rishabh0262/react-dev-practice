95474050ad825006e65cfe6c1387300e0e40cf39
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-03-23 22:12:00 +0530

    Alternate way to write 'context type'. (required condition in mentioned in the comment in 'ComponentE.js'. Also remeber the DRAWBACK : which is (1st- it only works with 'Class-Components'. 2nd- you can only subscribe to only single context using ContextType.

--- src/Components/ComponentE.js
+++ src/Components/ComponentE.js
@@ -3,6 +3,10 @@ import ComponentF from './ComponentF'
 import UserContext from './userContext'
 
 class ComponentE extends Component {
+
+  // (2nd way) if the project supports "public class field" Syntax
+  static contextType = UserContext
+
   render() {
     return (
       <div>
@@ -13,6 +17,6 @@ class ComponentE extends Component {
   }
 }
 
-ComponentE.contextType = UserContext
+// ComponentE.contextType = UserContext      // 1st way to use Context!
 
 export default ComponentE
