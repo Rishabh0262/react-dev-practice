151420e603bb6ec542d8f0467bd7854e9ba573d1
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-03-23 10:55:00 +0530

    We used 'Default Value' where we passed it in React.createContext -> parameter.  & removed the 'UserProvider' tag from the 'App.js'. and duhhh.... We get the default value at 'ComponentF.js'.

---
+++ src/Components/userContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UserContext = React.createContext("React Developer...(this is the default value)");   // default value, as parameter
+
+const UserProvider = UserContext.Provider
+const UserConsumer = UserContext.Consumer
+
+export {UserProvider, UserConsumer}
