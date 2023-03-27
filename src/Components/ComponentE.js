6421d82145ff27228f84b422b923a7ba0f360fa9
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-03-28 01:03:56 +0530

    2nd way of writing context-type in case the project supports 'public class field' syntax. Also I've mentioned the example for using-complexity of using context-type for consuming_multiple_context.

--- src/Components/ComponentE.js
+++ src/Components/ComponentE.js
@@ -12,7 +12,7 @@ class ComponentE extends Component {
       <div>
         Component E context {this.context}
         <ComponentF />
-        </div>
+      </div>
     )
   }
 }
@@ -20,3 +20,17 @@ class ComponentE extends Component {
 // ComponentE.contextType = UserContext      // 1st way to use Context!
 
 export default ComponentE
+
+/*      *** DEMERIT of Using ContextType ***  example...
+  return (
+    <ThemeContext.Consumer>
+      {theme => (
+        <UserContext.Consumer>
+          {user => (
+            <ProfilePage user={user} theme={theme} />       // just to use this line, we need to get nested this much.
+          )}
+        </UserContext.Consumer>
+      )}
+    </ThemeContext.Consumer>
+  );
+*/
